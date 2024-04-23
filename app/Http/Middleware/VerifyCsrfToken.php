a254757a7c917d93844825180f04ed30151420e2
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-04-23 11:23:19 +0400

    revrite token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,7 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
       'http://127.0.0.1:8000/*',
       'http://localhost:8000/*',
-      'http://http://milioninfo.webex.am/*',
-      'https://http://milioninfo.webex.am/*'
+      'http://milioninfo.webex.am/*',
+      'https://milioninfo.webex.am/*'
   ];
 }
