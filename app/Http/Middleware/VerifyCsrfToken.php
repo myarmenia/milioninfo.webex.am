e4fb0c3b83527a40057b35ea3f83663173ef499e
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-04-23 11:20:07 +0400

    add verify token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,5 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
       'http://127.0.0.1:8000/*',
       'http://localhost:8000/*',
+      'http://http://milioninfo.webex.am/*',
+      'https://http://milioninfo.webex.am/*'
   ];
 }
