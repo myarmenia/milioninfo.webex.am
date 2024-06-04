06c1e91fec5c9340cbaf4a4a912756cdbd06236d
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-06-04 13:04:34 +0400

    add file path

--- app/Http/Resources/OrganizationImagesResource.php
+++ app/Http/Resources/OrganizationImagesResource.php
@@ -14,6 +14,11 @@ class OrganizationImagesResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+
+        return [
+          "id"=>$this->id,
+          "organization_id"=>$this->organization_id,
+          "image_path" =>route('get-file',['path'=>$this->image_path])
+        ];
     }
 }
