12a24d0ba8abfa6e364c74a1f5940135444c5cef
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-06-04 17:26:39 +0400

    add time

--- app/Http/Resources/NearestBranchResource.php
+++ app/Http/Resources/NearestBranchResource.php
@@ -25,7 +25,6 @@ class NearestBranchResource extends JsonResource
         "latitude" =>$this->latitude,
         "longitude"=>$this->longitude,
         "work_time"=>$this->working_time(),
-        // "opend_status"=>$this->getWeekDays($this->working_time()),
         "opend_status"=>$this->checkTime($this->work_time_en),
         "title"=>$this->title(),
 
@@ -40,79 +39,122 @@ class NearestBranchResource extends JsonResource
 
     public function checkTime($string)
     {
+      // dd($string);
+        // $string="Mon Tue Wed Thu Fri Sat 10:00-18:00";
+        // $string="Mon Tue Wed Thu Fri 09:00-19:00Sat 09:00-18:00";
+        // $string="Mon Tue Wed Thu Fri Sat Sun 24 hours";
         // dd($string);
         // Get the current day and time
         $now = Carbon::now();
         $day = $now->format('D'); // e.g., Mon, Tue, Wed, etc.
         $time = $now->format('H:i'); // Current time in HH:MM format
 
-        $allowedDays=explode(' ',$string);
-
-
+        $allowedDays = explode(' ',$string);
         $startTime = '';
         $endTime = '';
         $opend_status='';
-        // dd($day, $allowedDays);
-        // dd($day);
-        if (in_array($day, $allowedDays)) {
-            // dd(777);
-            // dd($string);
-          if (Str::contains($string, "-")) {
-            // dd(888);
-            $string_explode=explode('-',$string);
-            // dd($string_explode[0]);
-            $pattern = '/\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b/';
-            preg_match_all($pattern, $string_explode[0], $matches);
+// dd($allowedDays[5]);
+        if(preg_match('/(\d{2}:\d{2})-(\d{2}:\d{2})/', $allowedDays[5], $matches)){
+          // dd($allowedDays[5]);
 
-            $startTime = $matches[0][0];
+          return $this->check_string($string, $opend_status);
+        }else{
 
-            $endTime=$string_explode[1];
-            // dd($time,$startTime,$endTime);
-            // Check if the current time is within the allowed range
-              if ($time >= $startTime && $time <= $endTime) {
-                // // return 'Current time is within the allowed range.';
-                // dd(778);
+          if (in_array($day, $allowedDays)) {
 
-                if(app()->getLocale()=='am'){
-                  return $opend_status="Բաց է";
-                }
-                elseif(app()->getLocale()=='ru'){
-                  return $opend_status="Открыть";
+            if (Str::contains($string, "-")) {
+              // dd(888);
+              $string_explode  =explode('-',$string);
+
+              // dd($string_explode[0]);
+              $pattern = '/\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b/';
+              preg_match_all($pattern, $string_explode[0], $matches);
+
+              $startTime = $matches[0][0];
+
+              $endTime = $string_explode[1];
+              // dd($time,$startTime,$endTime);
+              // Check if the current time is within the allowed range
+                if ($time >= $startTime && $time <= $endTime) {
+                  return $this->opened();
+
+                  // if(app()->getLocale()=='am'){
+                  //   return $opend_status="Բաց է";
+                  // }
+                  // elseif(app()->getLocale()=='ru'){
+                  //   return $opend_status="Открыть";
+                  // }else{
+
+                  //   return $opend_status="Open";
+                  // }
+
                 }else{
-                  return $opend_status="Open";
+                  return $this->closed();
+
+                  // if(app()->getLocale()=='am'){
+                  //   return $opend_status="Փակ է";
+                  // }
+                  // elseif(app()->getLocale()=='ru'){
+                  //   return $opend_status="Закрывать";
+                  // }else{
+                  //   return $opend_status="Close";
+                  // }
+
                 }
 
-              }else{
+            }
+            if (Str::contains($string, 24)) {
 
-                if(app()->getLocale()=='am'){
-                  return $opend_status="Փակ է";
-                }
-                elseif(app()->getLocale()=='ru'){
-                  return $opend_status="Закрывать";
-                }else{
-                  return $opend_status="Close";
-                }
+              return $this->opened();
 
-              }
+              // if(app()->getLocale()=='am'){
+              //   return $opend_status="Բաց է";
+              // }
+              // elseif(app()->getLocale()=='ru'){
+              //   return $opend_status="Открыть";
+              // }else{
+              //   return $opend_status="Open";
+              // }
+
+            }
+
 
           }
-          if (Str::contains($string, 24)) {
+      }
 
-            if(app()->getLocale()=='am'){
-              return $opend_status="Բաց է";
-            }
-            elseif(app()->getLocale()=='ru'){
-              return $opend_status="Открыть";
-            }else{
-              return $opend_status="Open";
-            }
-
-          }
-
-
-        }
 
         return $opend_status=null;
     }
+    public function opened(){
 
+      if(app()->getLocale()=='am'){
+        return $opend_status="Բաց է";
+      }
+      elseif(app()->getLocale()=='ru'){
+        return $opend_status="Открыть";
+      }else{
+        return $opend_status="Open";
+      }
+
+    }
+    public function closed(){
+
+      if(app()->getLocale()=='am'){
+        return $opend_status="Փակ է";
+      }
+      elseif(app()->getLocale()=='ru'){
+        return $opend_status="Закрывать";
+      }else{
+        return $opend_status="Close";
+      }
+
+
+    }
+
+    public function check_string($string,$opend_status){
+dd($opend_status);
+        dd(5559);
+        return $opend_status=null;
+
+    }
 }
