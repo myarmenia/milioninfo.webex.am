05aea15a115de803735dedd7e6d080be90ca2fa7
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-06-05 14:54:08 +0400

    add time for sut

--- app/Http/Resources/NearestBranchResource.php
+++ app/Http/Resources/NearestBranchResource.php
@@ -14,147 +14,183 @@ class NearestBranchResource extends JsonResource
      *
      * @return array<string, mixed>
      */
+
+
     public function toArray(Request $request): array
     {
 
-      return  [
-        "id"=>$this->id,
-        "organization_id"=>$this->organization_id,
-        "address"=>$this->address(),
-        "telephone"=>$this->telephone,
-        "latitude" =>$this->latitude,
-        "longitude"=>$this->longitude,
-        "work_time"=>$this->working_time(),
-        "opend_status"=>$this->checkTime($this->work_time_en),
-        "title"=>$this->title(),
+        return  [
+          "id"=>$this->id,
+          "organization_id"=>$this->organization_id,
+          "address"=>$this->address(),
+          "telephone"=>$this->telephone,
+          "latitude" =>$this->latitude,
+          "longitude"=>$this->longitude,
+          "work_time"=>$this->working_time()=="null" ? null : $this->working_time(),
+          "opend_status"=>$this->checkTime($this->work_time_en,$this->id),
+          "title"=>$this->title(),
 
-    ];
-
-
-
-
+      ];
 
     }
 
 
-    public function checkTime($string)
+    public function checkTime($string,$id)
     {
       // dd($string);
         // $string="Mon Tue Wed Thu Fri Sat 10:00-18:00";
         // $string="Mon Tue Wed Thu Fri 09:00-19:00Sat 09:00-18:00";
         // $string="Mon Tue Wed Thu Fri Sat Sun 24 hours";
+        $string="Mon Tue Wed Thu Fri Sat Sun 10:30-20:30";
         // dd($string);
+        // =============
         // Get the current day and time
         $now = Carbon::now();
         $day = $now->format('D'); // e.g., Mon, Tue, Wed, etc.
         $time = $now->format('H:i'); // Current time in HH:MM format
 
-        $allowedDays = explode(' ',$string);
+        $allowedDays = explode(' ', $string);
+        // dd( $allowedDays);
         $startTime = '';
         $endTime = '';
-        $opend_status='';
 // dd($allowedDays[5]);
-        if(preg_match('/(\d{2}:\d{2})-(\d{2}:\d{2})/', $allowedDays[5], $matches)){
-          // dd($allowedDays[5]);
+        if(isset($allowedDays[5])){
+          // dd(55);
+          $check_number_in_string = $this->containsNumber($allowedDays[5]);
 
-          return $this->check_string($string, $opend_status);
-        }else{
+            if($check_number_in_string==true){
+                // dd(66);
+               return  $this->check_string_with_time($string,$id);
 
-          if (in_array($day, $allowedDays)) {
+            }else{
+              // dd(77);
 
-            if (Str::contains($string, "-")) {
-              // dd(888);
-              $string_explode  =explode('-',$string);
+                  if (in_array($day, $allowedDays)) {
 
-              // dd($string_explode[0]);
-              $pattern = '/\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b/';
-              preg_match_all($pattern, $string_explode[0], $matches);
+                    if (Str::contains($string, "-")) {
+                      // dd(888);
+                      $string_explode  =explode('-',$string);
+                      // dd($string_explode);
+                      // dd($string_explode[0]);
+                      $pattern = '/\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b/';
+                      preg_match_all($pattern, $string_explode[0], $matches);
+                      // dd($matches);
+                      $startTime = $matches[0][0];
 
-              $startTime = $matches[0][0];
+                      $endTime = $string_explode[1];
 
-              $endTime = $string_explode[1];
-              // dd($time,$startTime,$endTime);
-              // Check if the current time is within the allowed range
-                if ($time >= $startTime && $time <= $endTime) {
-                  return $this->opened();
+                      // Check if the current time is within the allowed range
+                        if ($time >= $startTime && $time <= $endTime) {
 
-                  // if(app()->getLocale()=='am'){
-                  //   return $opend_status="Բաց է";
-                  // }
-                  // elseif(app()->getLocale()=='ru'){
-                  //   return $opend_status="Открыть";
-                  // }else{
+                          return $this->opened();
 
-                  //   return $opend_status="Open";
-                  // }
+                        }else{
 
-                }else{
+                          return $this->closed();
+                        }
+
+                    }
+                    if (Str::contains($string, 24)) {
+
+                      return $this->opened();
+
+                    }
+
+                  }
                   return $this->closed();
 
-                  // if(app()->getLocale()=='am'){
-                  //   return $opend_status="Փակ է";
-                  // }
-                  // elseif(app()->getLocale()=='ru'){
-                  //   return $opend_status="Закрывать";
-                  // }else{
-                  //   return $opend_status="Close";
-                  // }
-
-                }
-
             }
-            if (Str::contains($string, 24)) {
+        }
 
-              return $this->opened();
 
-              // if(app()->getLocale()=='am'){
-              //   return $opend_status="Բաց է";
-              // }
-              // elseif(app()->getLocale()=='ru'){
-              //   return $opend_status="Открыть";
-              // }else{
-              //   return $opend_status="Open";
-              // }
 
+
+    }
+
+    function containsNumber($string) {
+      // Regular expression to match any number
+      return preg_match('/\d/', $string) === 1;
+  }
+
+    public function check_string_with_time($string,$id){
+        // dd($string,$id);
+        $now = Carbon::now();
+        $day = $now->format('D'); // e.g., Mon, Tue, Wed, etc.
+        $time = $now->format('H:i'); // Current time in HH:MM format
+        // $opend_status='';
+        // $day="Sun";
+
+        $allowedDays = ["Mon","Tue", "Wed","Thu","Fri","Sat"];
+
+        $string_explode = explode('Sat',$string);
+
+        $pattern = '/\b([01]?[0-9]|2[0-3]):[0-5][0-9]\b/';
+        if (in_array($day, $allowedDays)) {
+          if($day!="Sat" ||  $day!="Sun"){
+
+            preg_match_all($pattern, $string_explode[0], $matches);
+            // dd($matches);
+            $startTime = $matches[0][0];
+            // dd($matches[0]);
+            $endTime = $matches[0][1];
+            // dd($startTime,$endTime);
+
+            return $this->check_time_opend_cloced($time,$startTime,$endTime);
+
+
+          }
+          if($day=="Sat"){
+            // dd($id);
+              // dd($string_explode[1]);
+              $explode_second_part = explode("-",$string_explode[1]);
+              $startTime = $explode_second_part[0];
+              $endTime = $explode_second_part[1];
+              // dd(77);
+              return $this->check_time_opend_cloced($time,$startTime,$endTime);
+          }
+        }else{
+          return $this->closed();
+        }
+
+    }
+    public function check_time_opend_cloced($time,$startTime,$endTime){
+
+      if ($time >= $startTime && $time <= $endTime) {
+
+        return $this->opened();
+
+      }else{
+
+        return $this->closed();
+      }
+
+    }
+
+    public function opened(){
+      // dd(77);
+            if(app()->getLocale()=='am'){
+              return "Բաց է";
+            }
+            elseif(app()->getLocale()=='ru'){
+              return "Открыть";
+            }else{
+              return "Open";
+            }
+
+          }
+          public function closed(){
+
+            if(app()->getLocale()=='am'){
+              return "Փակ է";
+            }
+            elseif(app()->getLocale()=='ru'){
+              return "Закрыто";
+            }else{
+              return "Close";
             }
 
 
           }
-      }
 
 
-        return $opend_status=null;
-    }
-    public function opened(){
-
-      if(app()->getLocale()=='am'){
-        return $opend_status="Բաց է";
-      }
-      elseif(app()->getLocale()=='ru'){
-        return $opend_status="Открыть";
-      }else{
-        return $opend_status="Open";
-      }
-
-    }
-    public function closed(){
-
-      if(app()->getLocale()=='am'){
-        return $opend_status="Փակ է";
-      }
-      elseif(app()->getLocale()=='ru'){
-        return $opend_status="Закрывать";
-      }else{
-        return $opend_status="Close";
-      }
-
-
-    }
-
-    public function check_string($string,$opend_status){
-dd($opend_status);
-        dd(5559);
-        return $opend_status=null;
-
-    }
 }
