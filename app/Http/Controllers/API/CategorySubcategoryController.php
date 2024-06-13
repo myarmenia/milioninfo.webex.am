5bfd60dbd11f4860fc8cc28e38435a1d6c32844b
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-06-13 13:10:37 +0400

    change count

--- app/Http/Controllers/API/CategorySubcategoryController.php
+++ app/Http/Controllers/API/CategorySubcategoryController.php
@@ -12,7 +12,7 @@ use App\Models\Branch;
 use App\Models\Category;
 use App\Models\Organization;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 
 class CategorySubcategoryController extends BaseController
 {
@@ -35,16 +35,37 @@ class CategorySubcategoryController extends BaseController
 
       $organization_ids = Organization::whereIn('subcategory_id',$subcategories_id)->pluck('id');
 
-      $data = Branch::whereIn('organization_id',$organization_ids);
-      if($latitude!=null && $longitude!=null){
+      // $data = Branch::whereIn('organization_id',$organization_ids);
+      // if($latitude!=null && $longitude!=null){
 
-        $data=$data->where('latitude', '<=', $coordinate['latitude'])
-        ->where('longitude', '<=', $coordinate['longitude']);
-      }
+      //   $data = $data->where('latitude', '<=', $coordinate['latitude'])
+      //   ->where('longitude', '<=', $coordinate['longitude']);
+      // }
 
-        $data=$data->paginate(30)->withQueryString();
+      //   $data = $data->paginate(30)->withQueryString();
 
 
-        return $this->sendResponse(BranchWithOrganizationResource::collection($data),'success', ['page_count' => $data->lastPage()]);
+      //   return $this->sendResponse(BranchWithOrganizationResource::collection($data),'success', ['page_count' => $data->lastPage()]);
+      // $radius = $request->input('radius', 1); // Радиус поиска по умолчанию (10 км)
+      $distance = 1.0; // 1.0 kilometer, which equals 1000 meters
+      $data = Branch::whereIn('organization_id', $organization_ids);
+
+        if ($latitude !== null && $longitude !== null) {
+            $data = $data->select(
+                'branches.*',
+                DB::raw("6371 * acos(cos(radians($latitude))
+                * cos(radians(latitude))
+                * cos(radians(longitude) - radians($longitude ))
+                + sin(radians($latitude))
+                * sin(radians(latitude))) AS distance")
+            )
+            ->havingRaw('distance >= ?', [$distance])
+            ->orderBy('distance');
+        }
+
+        $data = $data->paginate(30)->withQueryString();
+      return $this->sendResponse(BranchWithOrganizationResource::collection($data),'success', ['page_count' => $data->lastPage()]);
+
+
     }
 }
