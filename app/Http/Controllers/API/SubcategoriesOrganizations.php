a10ede66ad006947f46ace42a0de6d47d855f7b8
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-06-17 09:34:51 +0400

    777

--- app/Http/Controllers/API/SubcategoriesOrganizations.php
+++ app/Http/Controllers/API/SubcategoriesOrganizations.php
@@ -10,6 +10,7 @@ use App\Models\Branch;
 use App\Models\Organization;
 use App\Models\Subcategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SubcategoriesOrganizations extends BaseController
 {
@@ -25,13 +26,26 @@ class SubcategoriesOrganizations extends BaseController
       //   return $this->sendResponse(OrganizationsBranchesResource::collection($data),'success',['page_count' => $data->lastPage()]);
       // }
       $organization_ids = Organization::whereIn('subcategory_id',$subcategory)->pluck('id');
-
+      $distance = 1.0; // 1.0 kilometer, which equals 1000 meters
       $data=Branch::whereIn('organization_id',$organization_ids);
-      if($latitude!=null && $longitude!=null){
+      // if($latitude!=null && $longitude!=null){
 
-        $data=$data->where('latitude', '<=', $coordinate['latitude'])
-        ->where('longitude', '<=', $coordinate['longitude']);
-      }
+      //   $data=$data->where('latitude', '<=', $coordinate['latitude'])
+      //   ->where('longitude', '<=', $coordinate['longitude']);
+      // }
+      if ($latitude !== null && $longitude !== null) {
+        $data = $data->select(
+            'branches.*',
+            DB::raw("6371 * acos(cos(radians($latitude))
+            * cos(radians(latitude))
+            * cos(radians(longitude) - radians($longitude ))
+            + sin(radians($latitude))
+            * sin(radians(latitude))) AS distance")
+        )
+        ->havingRaw('distance >= ?', [$distance])
+        ->orderBy('distance');
+    }
+
 
         $data=$data->paginate(30)->withQueryString();
 
