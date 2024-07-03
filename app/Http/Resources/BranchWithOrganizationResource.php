9c2e34c2b9e4690f029c5c7d778a0289e5b381c5
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-07-03 11:57:27 +0400

    add category_ids

--- app/Http/Resources/BranchWithOrganizationResource.php
+++ app/Http/Resources/BranchWithOrganizationResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CategorySubcategory;
 use App\Traits\CheckOpendClosedTrait;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -18,7 +19,8 @@ class BranchWithOrganizationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-
+      
+// dd($this->organizations->subcategories->categories);
          return [
 
           "id"=>$this->id,
--- app/Http/Resources/OrganizationsBranchResource.php
+++ app/Http/Resources/OrganizationsBranchResource.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Resources;
 
+use App\Models\CategorySubcategory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
 
 class OrganizationsBranchResource extends JsonResource
 {
@@ -15,13 +17,16 @@ class OrganizationsBranchResource extends JsonResource
     public function toArray(Request $request): array
     {
 
+      $category_ids = DB::table('categories_subcategories')->where('subcategory_id',$this->subcategory_id)->pluck('category_id');
+
         return [
-          'id'=>$this->id,
-          'name'=>$this->translation(),
-          'subcategory_id'=>$this->subcategory_id,
-          'subcategory'=>$this->subcategories->translation(),
-          'weblinks'=>new WeblinkResource($this->weblinks=="null" ? null : $this->weblinks),
-          'images'=>OrganizationImagesResource::collection($this->images ?? null),
+          'id' => $this->id,
+          'name' => $this->translation(),
+          'subcategory_id' => $this->subcategory_id,
+          'subcategory' => $this->subcategories->translation(),
+          'category_ids' => $category_ids,
+          'weblinks' => new WeblinkResource($this->weblinks=="null" ? null : $this->weblinks),
+          'images' => OrganizationImagesResource::collection($this->images ?? null),
         ];
     }
 }
