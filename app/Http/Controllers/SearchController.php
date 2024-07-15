37b98c9af9ace1b3a14408bcdbbc9360e85f6385
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-07-15 10:25:07 +0400

    add search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -32,16 +32,16 @@ class SearchController extends BaseController
       // $distance = 1.0;
       $distance = 0;
       // $data = Branch::search($searched_word,$latitude, $longitude);
-      $data = Branch::search($searched_word);
-      $subcategoryResults = Subcategory::search($searched_word)->first();
-      // dd($subcategoryResults);
-      if($subcategoryResults!=null){
+      // $data = Branch::search($searched_word);
+      // $subcategoryResults = Subcategory::search($searched_word)->first();
+      // // dd($subcategoryResults);
+      // if($subcategoryResults!=null){
 
-         $subcategoryResults = $subcategoryResults->organizations->pluck('branches')->flatten()->toArray();
+      //    $subcategoryResults = $subcategoryResults->organizations->pluck('branches')->flatten()->toArray();
 
-      }
+      // }
       $searched_words = explode(',', $searched_word); // преобразуем строку в массив
-      $subcategoryResults= Subcategory::where(function($query) use ( $searched_words) {
+      $subcategoryResults = Subcategory::where(function($query) use ( $searched_words) {
         foreach ($searched_words as $word) {
             $query->orWhere(function($query) use ($word) {
                 $query->where('subCategory_am', 'like', '%' . $word . '%')
@@ -52,13 +52,11 @@ class SearchController extends BaseController
     })->get();
 
 // dd($subcategoryResults);
- $branch_ids_array=[];
-// dd($subcategoryResults->pluck('organizations')->flatten()->pluck('branches')->flatten()->pluck('id'));
+ $branch_ids_array = [];
+ $subcategoryResults = $subcategoryResults->pluck('organizations')->flatten()->pluck('branches')->flatten()->pluck('id')->toArray();
 
 
-// $subcategoryResults = $subcategoryResults->organizations->pluck('branches')->flatten()->toArray();
-// $subcategoryResults = $subcategoryResults->organizations->pluck('branches')->flatten()->pluck('id');
-// dd($subcategoryResults);
+
       // $organizationResults = Organization::search($searched_word)->get();
       // dd($organizationResults);
 
@@ -75,10 +73,10 @@ class SearchController extends BaseController
 // dd($organizationResults);
       // $organizationResults = $organizationResults->pluck('branches')->flatten()->toArray();
       // dd($organizationResults);
-      $organizationResult = $organizationResults->pluck('branches')->flatten()->pluck('id');
+      $organizationResults = $organizationResults->pluck('branches')->flatten()->pluck('id')->toArray();
       // dd($organizationResult);
 
-      $branchResults = Branch::search($searched_word)->get()->toArray();
+      // $branchResults = Branch::search($searched_word)->get()->toArray();
       $searched_words = explode(',', $searched_word); // преобразуем строку в массив
       $branchResults = Branch::where(function($query) use ( $searched_words) {
         foreach ( $searched_words as $word) {
@@ -93,33 +91,42 @@ class SearchController extends BaseController
             });
         }
     })->get();
+    // dd($branchResults);
+    $branchResults = $branchResults->pluck('id')->toArray();
 
-      dd($branchResults);
+    // $subcategoryResults1 = collect($subcategoryResults);
+    // dd($subcategoryResults1);
+    // $organizationResults1 = collect($organizationResults);
 
-      // $allResults = $branchResults->merge($organizationResults)->merge($subcategoryResults);
-      // $allResults = array_merge($branchResults,$organizationResults,$subcategoryResults);
+    // $branchResults1 = collect($branchResults);
+// dd($organizationResults1,$branchResults1);
+// dd($subcategoryResults,$organizationResult,$branchResults);
+      // $allResults = $branchResults1->merge($organizationResults1)->merge($subcategoryResults1);
+      // dd($allResults);
 
+      $allResults = array_merge($branchResults,$organizationResults,$subcategoryResults);
+// dd($allResults);
       // $ids = $allResults->pluck('id');
-      // $data = Branch::whereIn('id', $ids);
+      $data = Branch::whereIn('id', $allResults);
 
 // dd($data);
-    //   if ($latitude !== null && $longitude !== null) {
+      if ($latitude !== null && $longitude !== null) {
 
-    //     $data = $data->select(
-    //         'branches.*',
-    //         DB::raw("6371 * acos(cos(radians($latitude))
-    //         * cos(radians(latitude))
-    //         * cos(radians(longitude) - radians($longitude ))
-    //         + sin(radians($latitude))
-    //         * sin(radians(latitude))) AS distance")
-    //     )
-    //     ->havingRaw('distance >= ?', [$distance])
-    //     ->orderBy('distance');
-    // }
+        $data = $data->select(
+            'branches.*',
+            DB::raw("6371 * acos(cos(radians($latitude))
+            * cos(radians(latitude))
+            * cos(radians(longitude) - radians($longitude ))
+            + sin(radians($latitude))
+            * sin(radians(latitude))) AS distance")
+        )
+        ->havingRaw('distance >= ?', [$distance])
+        ->orderBy('distance');
+    }
 
       $data = $data->paginate(30)->withQueryString();
       // $branchResults
-      $uniqueMergedArray = $this->getUniqueMergedArray($subcategoryResults,$organizationResults);
+      // $uniqueMergedArray = $this->getUniqueMergedArray($subcategoryResults,$organizationResults);
       // dd($uniqueMergedArray);
       return $this->sendResponse(NewBranchResource::collection($data),'success',['page_count' => $data->lastPage()]);
       // return $this->sendResponse(NewBranchResource::collection($uniqueMergedArray),'success');
